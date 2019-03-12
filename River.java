e7d051c050d6e35b7ebc69f4f6ac7b17b1925503
dks11 <47439162+dks11@users.noreply.github.com>
2019-03-12 13:33:19 -0300

    River upates added
    Updates

--- River.java
+++ River.java
@@ -1,9 +1,49 @@
+import java.io.*;
+import java.util.Scanner;
 public class River
 {
 	private static int sum = 0;
 	public static void main(String[] args)
 	{
+		Scanner stdin;
+		BufferedReader br;
+		FileReader fileIn;
+		try
+		{	
+			fileIn = new FileReader(args[0]);
+			br = new BufferedReader(fileIn);
+			stdin = new Scanner(br);
+		}
 		
+		catch(FileNotFoundException e)
+		{
+			System.out.println(e.getMessage());
+			return;
+		}
+
+		int row = stdin.nextInt();
+		int col = stdin.nextInt();
+		int start = stdin.nextInt();
+		int[][] list = new int[row][col];
+
+		for(int i=0; i<row; i++)
+      		{
+           		for(int j=0; j<col; j++)
+           		{
+               			list[i][j] = stdin.nextInt();
+           		}
+       		}
+
+		System.out.println("The Size Of the River is " + find0(list,0,start));
+
+       		for(int i=0; i<row; i++)
+       		{
+           		for(int j=0; j<col; j++)
+           		{
+               			System.out.print(list[i][j]+ "  ");
+           		}
+           		System.out.println();
+		}
 	}
 	
 	private static int find0(int[][] list,int r,int c)
@@ -11,33 +51,31 @@ public class River
 		boolean done = false;
 		list[r][c] = 7;
 		
-	if()
-	{
-		return 1;
-	}
+		if( (r-1 >=0 && list[r-1][c] > 0) && (r+1 <= list.length && list[r+1][c] > 0) && (c-1 >= 0 && list[r][c-1] > 0)
+			&& (c+1 <= list[r].length && list[r][c+1] > 0) )
+		{
+			return sum + 1;
+		}
 	
-	if(r-1 >=0 && list[r-1][c] == 0)
-	{
-		return River.find0(list, r-1, c);
-	}
+		if(r-1 >=0 && list[r-1][c] == 0)
+		{
+			return sum + River.find0(list, r-1, c);
+		}
 	
-	if(r+1 <= list.length && list[r+1][c] == 0)
-	{
-		return River.find0(list, r+1, c);
-	}
+		if(r+1 <= list.length && list[r+1][c] == 0)
+		{
+			return sum + River.find0(list, r+1, c);
+		}
 	
-	if(c-1 >= 0 && list[r][c-1] == 0)
-	{
-		return River.find0(list, r, c-1);
-	}
+		if(c-1 >= 0 && list[r][c-1] == 0)
+		{
+			return sum + River.find0(list, r, c-1);
+		}
 	
-	if(c+1 <= list[r].length && list[r][c+1] == 0)
-	{
-		return River.find0(list, r, c+1);
-	}
-	
-	private static int change0(int[][] list,r,c)
-	{
-		list[r][c] = 7;
+		if(c+1 <= list[r].length && list[r][c+1] == 0)
+		{
+			return sum + River.find0(list, r, c+1);
+		}
+		return sum;
 	}
 }	
