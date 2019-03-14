ba27164f14d75f9f275ea082362d6bf2cb01ff8d
dks11 <47439162+dks11@users.noreply.github.com>
2019-03-14 14:20:10 -0300

    Updates
    Program works yay!

--- ZeroSum.java
+++ ZeroSum.java
@@ -3,10 +3,10 @@ public class ZeroSum
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Please enter 5 integers");
+		System.out.println("Please enter 3 integers");
 		Scanner stdin = new Scanner(System.in);
 	
-		int[] list = new int[5];
+		int[] list = new int[3];
 		for (int i = 0; i < list.length; i++)
 		{
 			list[i] = stdin.nextInt();
@@ -22,6 +22,9 @@ public class ZeroSum
 
 	public static boolean helper(int[] a, int i, int sum)
 	{
+		boolean bool1 = true; 
+		boolean bool2 = true;
+		
 		if (i == 0)
 		{
 			return false;
@@ -34,16 +37,24 @@ public class ZeroSum
 
 		if ( (i > 0) && (sum - a[i-1] != 0) )
 		{
-			sum += a[i-1];
-			return helper(a,i-1,sum);
-		}
-		
-		if ( (i > 0) && (sum-a[i-1] != 0) )
-		{
-			sum += a[i-1];
-			return helper(a,i-1,sum - a[i-1]);
+			System.out.println("This is the sum " + (sum - a[i-1])  );
+
+			bool1 = helper(a,i-1,sum);
+			
+			if(bool1)
+			{
+				return bool1;
+			}
+			
+			
+		    bool2  = helper(a,i-1,sum - a[i-1]);
+			
+			if(bool2)
+			{
+				return bool2;
+			}
 		}
 
- 		return true;
+ 		return (bool1 || bool2);
 	}
 }
